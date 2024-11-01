e25f58d02937c9f69858065e50529485be24a8b3
Nadeem-mohammad0021 <nadeemmohammad.0021@gmail.com>
2024-11-01 17:27:00 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -108,7 +108,7 @@
             this.addPosition(x, y);
         },
         canScale: function() {
-            return this.heart.scale > 0.2;
+            return this.heart.scale > 0.5;
         },
         setHeartScale: function(scale) {
             this.heart.scale *= scale;
